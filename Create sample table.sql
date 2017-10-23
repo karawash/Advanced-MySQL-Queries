d66c5091ffaa533ccf31d13e4761f9143dba3d23
karawash <ahmad_karawash@hotmail.com>
2017-10-22 20:15:15 -0400

    jjn
    jjhjh

---
+++ Create sample table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE test
+	(`name` varchar(7), `value` int);
+	
+INSERT INTO test
+	(`name`, `value`)
+VALUES
+	('Ahmad', 10),
+	('peter', 20),
+	('kevin', 30),
+    ('karim', 15),
+    ('andriy', 23),
+    ('hoa', 17),
+    ('yuzi', 19),
+    ('Denika', 22);
+
